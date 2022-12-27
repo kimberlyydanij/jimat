655c50a7fe01ca791469b0fe7cce50df6449b975
KimberlyDaniJ <kimberlydanij@gmail.com>
2022-12-27 21:48:56 +0900

    20221227 | Add

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
-function vita500(){
-    location.href = "/teamVita500.html";
-    
+function vita500() {
+  location.href = "/teamVita500.html";
 }
 
-function curationPage(){
-    location.href = "/curationPage.html";
-    
+function curationPage() {
+  location.href = "/curationPage.html";
 }
 
-function move_1km(){
-    location.href = "/1kmMap.html";
-    
+function move_1km() {
+  location.href = "/1kmMap.html";
 }
 
-function magazinePage(){
-    location.href = "/magazinePage.html";
-    
+function magazinePage() {
+  location.href = "/magazinePage.html";
 }
 
-function myPage(){
-    location.href = "/myPage.html";
-    
+function myPage() {
+  location.href = "/myPage.html";
 }
 
+function mainPage() {
+  location.href = "/index.html";
+}
