d50c13cab226fd4fd071060b01f2804d39bfbab8
KimberlyDaniJ <kimberlydanij@gmail.com>
2023-01-03 05:44:00 +0900

    20230103 | Update

--- js/main.js
+++ js/main.js
@@ -33,3 +33,12 @@ function sign_up() {
 function review() {
   location.href = "/review.html";
 }
+
+function dp_menu() {
+  let click = document.getElementById("drop-content");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
